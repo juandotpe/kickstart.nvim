87cedc3987b743a350583d170fa3366e6fe1bd29
Juan Posadas <jposadasc@gmail.com>
2023-11-28 13:11:36 +0800

    change lualine theme

--- init.lua
+++ init.lua
@@ -178,7 +178,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'everforest',
         component_separators = '|',
         section_separators = '',
       },
