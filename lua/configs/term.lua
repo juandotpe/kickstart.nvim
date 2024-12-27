ae8076beed1b99aa36495e40042bba3e76b6ff70
Juan Posadas <jposadasc@gmail.com>
2024-12-27 23:02:30 +0800

    Add window navigation mappings in terminal mode

--- lua/configs/term.lua
+++ lua/configs/term.lua
@@ -6,6 +6,12 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
     vim.cmd.startinsert()
+
+    -- Window navigation in terminal mode
+    vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h")
+    vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l")
+    vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j")
+    vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k")
   end
 })
 
